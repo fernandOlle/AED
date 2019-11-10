b6e9dc135312e2cf19650bcee1ab975a8ce07696
owzi <frolle@inf.ufpel.edu.br>
2019-11-09 22:56:30 -0300

    meu deus O_o

---
+++ vamos-pls/vamos-pls/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  vamos-pls
+//
+//  Created by Fernando R. Ollé on 09/11/19.
+//  Copyright © 2019 Fernando R. Ollé. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
